23bd74af720fc2806b66cffcae3e61b0d1a201c1
BatihanKaragulle <batihankrgulle@gmail.com>
2021-12-03 03:20:07 +0300

    Tilemap and Level Walls

--- Assets/Topcan/Movement.cs
+++ Assets/Topcan/Movement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    float speed = 30;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,8 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical")*speed*Time.deltaTime;
+        transform.Translate(moveAmount,0,steerAmount);
     }
 }
